f732ff5b023f7a29b2a45cc9d343b6bb68be36e2
goru47 <gorudocks@gmail.com>
2017-01-24 14:45:51 +0100

    hardcoding verwijderd en options toegevoegd

--- BP_BETA_0.1.py
+++ BP_BETA_0.1.py
@@ -2,14 +2,15 @@ import pygame
 import sys
 from pygame.locals import *
 import random
+import math
+from settings import *
 
 # initialize pygame
 pygame.init()
 pygame.font.init()
 
-#   set up resolution
-display_width = 1280
-display_height = 720
+
+
 
 #   Colors
 black = (0, 0, 0)
@@ -28,6 +29,7 @@ largeText = pygame.font.Font('freesansbold.ttf', 115)
 mediumText = pygame.font.Font('freesansbold.ttf', 60)
 smallText = pygame.font.Font('freesansbold.ttf', 20)
 
+#middle of the screen
 ox = int(display_width/2)
 oy = int(display_height/2)
 
@@ -40,14 +42,14 @@ pygame.display.set_caption('pygame test')
 clock = pygame.time.Clock()
 
 #   loading images and setting the right resolution
-bg_image = pygame.image.load("check.png") #bg1 is vervangen
+bg_image = pygame.image.load("bg1.jpg")
 bg_image = pygame.transform.scale(bg_image, (display_width, display_height))
 ship_img = pygame.image.load("boot4.png")
 
-
 # een boot class hebben we het liefst, maar dat kreeg ik niet voor elkaar.
 
-"""def ship(x, y):
+"""
+def ship(x, y):
     gamedisplay.blit(ship_img(x, y))
 
     if mousbutton_pressed == [1]
@@ -57,7 +59,8 @@ ship_img = pygame.image.load("boot4.png")
         Self.y = posy
 
 x = (display_width * 0.45)
-y = (display_height * 0.8)"""
+y = (display_height * 0.8)
+"""
 
 
 
@@ -76,6 +79,36 @@ class object:
     def move_left(self):
         self.ox = self.ox - 50
 '''
+
+class game:
+    def __init__(self):
+        pygame.init()
+        #resolution
+        dis_width  = 1280
+        dis_height = 720
+        resolution = (dis_width,dis_height)
+
+        self.screen = pygame.display.set_mode(resolution)
+
+        self.largetext = pygame.font.Font('freesansbold.ttf', 115)
+        self.mediumtext = pygame.font.Font('freesansbold.ttf', 60)
+        self.smalltext = pygame.font.Font('freesansbold.ttf', 20)
+
+
+
+#class boot:
+#    def __init__(self,x,y,hp,width,length):
+#        self.x = x
+#        self.y = y
+#        self.hp = hp
+#        self.width = width
+#        self.length = length
+#        self.
+#    def updoot:
+#        action = pygame.mouse.get_pos()
+#
+#        if x + width > action[0] > x and y + length:
+
 
 #   function to display button an make them interactive
 #   (message, x pos, y pos, width, height, inactive color, active color, textsize)
@@ -126,7 +159,7 @@ def text_objects(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
 
-def board_gen():
+def board_gen(display_width,display_height):
     t = 0
     x = display_width/5
     y = 0
@@ -156,19 +189,19 @@ def board_gen():
     gameloop()
 
 
-def main_menu():
+def main_menu(display_width,display_height):
     in_main_menu = True
     while in_main_menu:
         gamedisplay.blit(bg_image,(0, 0))
-        #  gamedisplay.fill(white)
 
-        TextSurf, TextRect = text_objects("BattlePort", largeText)
+
+        TextSurf, TextRect = text_objects("BattlePort", mediumText)
         TextRect.center = ((display_width/2), (display_height/2))
         gamedisplay.blit(TextSurf, TextRect)
 
-        button("Play", display_width/6, 450, 200, 120, blue, bright_blue, mediumText)
-        button("Quit", display_width/6*4, 450, 200, 120, red, bright_red, mediumText)
-        button("Options", display_width/6*2.4, 450, 250, 120, silver, dark_silver, mediumText)
+        button("Play", display_width/6, display_height/1.6, display_width/6.4, display_height/6, blue, bright_blue, smallText)
+        button("Quit", display_width/6*4, display_height/1.6, display_width/6.4, display_height/6, red, bright_red, smallText)
+        button("Options", display_width/6*2.4, display_height/1.6, display_width/5.12, display_height/6, silver, dark_silver, smallText)
 
         mouse = pygame.mouse.get_pos()
 
@@ -177,26 +210,25 @@ def main_menu():
 
         for event in pygame.event.get():
             if event.type == MOUSEBUTTONDOWN:
-                if (display_width/6) + 200 > mouse[0] > (display_width/6) and 450 + 120 > mouse[1] > 450:
-                    board_gen()
-                if (display_width/6*4) + 200 > mouse[0] > (display_width/6*4) and 450+120 > mouse[1] > 450:
+                if (display_width/6) + display_width/6.4 > mouse[0] > (display_width/6) and display_height/1.6 + display_height/6 > mouse[1] > display_height/1.6:
+                    board_gen(display_width,display_height)
+                if (display_width/6*4) + display_width/6.4 > mouse[0] > (display_width/6*4) and display_height/1.6+display_height/6 > mouse[1] > display_height/1.6:
                     pygame.quit()
                     sys.exit()
-                if (display_width/6*2.4) + 200 > mouse[0] > (display_width/6*2.4) and 450 + 120 > mouse[1] > 450:
-                    pass # moet nog options menu makne
+                if (display_width/6*2.4) + display_width/5.12 > mouse[0] > (display_width/6*2.4) and display_height/1.6+ display_height/6 > mouse[1] > display_height/1.6:
+                    options(display_width,display_height) # moet nog options menu makne
 
-def paused():
+def paused(display_width,display_height):
     pause = True
     while pause:
-        # gamedisplay.fill(white)
 
         TextSurf, TextRect = text_objects("Pause", largeText)
         TextRect.center = ((display_width / 2), (display_height / 2))
         gamedisplay.blit(TextSurf, TextRect)
 
-        button("Play", display_width / 6, 450, 200, 120, blue, bright_blue, mediumText)
-        button("Quit", display_width / 6 * 4, 450, 200, 120, red, bright_red, mediumText)
-        button("Main menu", display_width / 6 * 2.21, 450, 330, 120, silver, dark_silver, mediumText)
+        button("Play", display_width / 6, display_height/1.6, display_width/6.4, display_height/6, blue, bright_blue, mediumText)
+        button("Quit", display_width / 6 * 4, display_height/1.6 , display_width/6.4, display_height/6, red, bright_red, mediumText)
+        button("Main menu", display_width / 6 * 2.21, display_height/1.6, display_width/3.87, display_height/6, silver, dark_silver, mediumText)
 
         mouse = pygame.mouse.get_pos()
 
@@ -206,16 +238,73 @@ def paused():
 
         for event in pygame.event.get():
             if event.type == MOUSEBUTTONDOWN:
-                if (display_width / 6) + 200 > mouse[0] > (display_width / 6) and 450 + 120 > mouse[1] > 450:
-                    board_gen()
-                if (display_width / 6 * 4) + 200 > mouse[0] > (display_width / 6 * 4) and 450 + 120 > mouse[1] > 450:
+                if (display_width / 6) + 200 > mouse[0] > (display_width / 6) and display_height/1.6 + display_height/6 > mouse[1] > display_height/1.6:
+                    board_gen(display_width,display_height)
+                if (display_width / 6 * 4) + 200 > mouse[0] > (display_width / 6 * 4) and display_height/1.6 + display_height/6 > mouse[1] > display_height/1.6:
                     pygame.quit()
                     sys.exit()
-                if (display_width/6*2.4) + 200 > mouse[0] > (display_width/6*2.4) and 450 + 120 > mouse[1] > 450:
-                    main_menu()
+                if (display_width/6*2.4) + 200 > mouse[0] > (display_width/6*2.4) and display_height/1.6 + display_height/6 > mouse[1] > display_height/1.6:
+                    main_menu(display_width,display_height)
+
+def options(display_width,display_height):
+    options = True
+    while options:
+        gamedisplay.fill(aqua)
+        TextSurf, TextRect = text_objects("options", mediumText)
+        TextRect.center = ((display_width / 2), (display_height / 10))
+        gamedisplay.blit(TextSurf, TextRect)
+
+        button("resolution", display_width/24, display_height/6, display_width/6, display_height/6, red, red, smallText)
+        button("480p", display_width/8*2, display_height/6, display_width/6, display_height/6, silver, dark_silver, smallText)
+        button("720p", display_width/8*4, display_height/6, display_width/6, display_height/6, silver, dark_silver, smallText)
+        button("1080p", display_width/8*6,display_height/6, display_width/6, display_height/6, silver, dark_silver, smallText)
+
+        button("sound", display_width/24, display_height/6*2.5, display_width/6, display_height/6, red, red, smallText)
+        button("off", display_width/8*2, display_height/6*2.5, display_width/6, display_height/6, silver, dark_silver, smallText)
+        button("50%", display_width/8*4, display_height/6*2.5, display_width/6, display_height/6, silver, dark_silver, smallText)
+        button("100%", display_width/8*6,display_height/6*2.5, display_width/6, display_height/6, silver, dark_silver, smallText)
+
+        button("window/full", display_width/8, display_height/6*4, display_width/4, display_height/6, silver, dark_silver, smallText)
+        button("main menu", display_width/8*3, display_height/6*4, display_width/4, display_height/6, silver, dark_silver, smallText)
+        button("exit", display_width/8*5,display_height/6*4, display_width/4, display_height/6, silver, dark_silver, smallText)
 
 
-def gameloop():
+
+        mouse = pygame.mouse.get_pos()
+        pygame.display.update()
+        clock.tick(15)
+
+        for event in pygame.event.get():
+            if event.type == MOUSEBUTTONDOWN:
+                if (display_width/8*2)+display_width/6 > mouse[0] > (display_width/8*2) and display_height/6 + display_height/6 > mouse[1] > display_height/6:
+                    pygame.transform.scale(gamedisplay,(640,480))
+                    pygame.display.set_mode((640,480))
+                    display_height= 480
+                    display_width = 640
+                if (display_width/8*4)+display_width/6 > mouse[0] > (display_width/8*4) and display_height/6 + display_height/6 > mouse[1] > display_height/6:
+                    pygame.display.set_mode((1280,720))
+                    pygame.transform.scale(gamedisplay,(1280,720))
+                    display_width = 1280
+                    display_height = 720
+                if (display_width/8*6)+display_width/6 > mouse[0] > (display_width/8*6) and display_height/6 + display_height/6 > mouse[1] > display_height/6:
+                    pygame.display.set_mode((1920,1080))
+                    pygame.transform.scale(gamedisplay,(1920,1080))
+                    display_width = 1920
+                    display_height = 1080
+                if (display_width/8*2)+display_width/6 > mouse[0] > (display_width/8*2) and display_height/6*2.5 + display_height/6 > mouse[1] > display_height/6*2.5:
+                    pass # Sounds options worden later toegevoegd
+                if (display_width/8*4)+display_width/6 > mouse[0] > (display_width/8*4) and display_height/6*2.5 + display_height/6 > mouse[1] > display_height/6*2.5:
+                    pass # Sounds options worden later toegevoegd
+                if (display_width/8*6)+display_width/6 > mouse[0] > (display_width/8*6) and display_height/6*2.5 + display_height/6 > mouse[1] > display_height/6*2.5:
+                    pass # Sounds options worden later toegevoegd
+                if (display_width/8)+display_width/4 > mouse[0] > (display_width/8) and display_height/6*4 + display_height/6 > mouse[1] > display_height/6*4:
+                    pygame.display.set_mode(fullscreen)
+                if (display_width/8*3)+display_width/4 > mouse[0] > (display_width/8*3) and display_height/6*4 + display_height/6 > mouse[1] > display_height/6*4:
+                    main_menu(display_width,display_height)
+                if (display_width/8*5)+display_width/4 > mouse[0] > (display_width/8*5) and display_height/6*4 + display_height/6 > mouse[1] > display_height/6*4:
+                    pygame.quit()
+
+def gameloop(display_width,display_height):
     crashed = False
 
     while not crashed:
@@ -235,4 +324,5 @@ def gameloop():
     clock.tick(5)
 
 
-main_menu()
+main_menu(display_width,display_height)
+
