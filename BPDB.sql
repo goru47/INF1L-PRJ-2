7587394a887bafbb3f1cef87cb68b7b211472afd
Joris-de-Putter <joris.putter@gmail.com>
2017-02-03 10:03:21 +0100

    Add files via upload

---
+++ BPDB.sql
@@ -0,0 +1,8 @@
+﻿DROP TABLE IF EXISTS scoreboard;
+
+create table scoreboard (naam varchar(20), score integer);
+
+insert into scoreboard(naam,score) values
+('Han', 50);
+
+select * from scoreboard
